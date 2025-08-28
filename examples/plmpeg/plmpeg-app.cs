a6345d5191b82ffee5cb4e4f549df7dafb6ac451
Eli Aloni <aloni.eli@gmail.com>
2025-08-28 15:37:54 +0300

    split plmpeg-app to several files, ring-buffer.cs , should be more readable

--- examples/plmpeg/plmpeg-app.cs
+++ examples/plmpeg/plmpeg-app.cs
@@ -362,7 +362,7 @@ public static unsafe partial class PlMpegApp
         {
             plm_buffer_destroy(state.plm_buffer);
         }
-        
+
         sg_shutdown();
 
         if (Debugger.IsAttached)
@@ -380,56 +380,6 @@ public static unsafe partial class PlMpegApp
         }
     }
 
-    //=== a simple ring buffer implementation ====================================*/
-    static uint ring_wrap(uint i)
-    {
-        return i % RING_NUM_SLOTS;
-    }
-
-    static bool ring_full(ring_t rb)
-    {
-        return ring_wrap(rb.head + 1) == rb.tail;
-    }
-
-    static bool ring_empty(ring_t rb)
-    {
-        return rb.head == rb.tail;
-    }
-
-    static uint ring_count(ring_t rb)
-    {
-        uint count;
-        if (rb.head >= rb.tail)
-        {
-            count = rb.head - rb.tail;
-        }
-        else
-        {
-            count = (rb.head + RING_NUM_SLOTS) - rb.tail;
-        }
-        return count;
-    }
-
-    static void ring_enqueue(ring_t rb, int val)
-    {
-        // assert(!ring_full(rb));
-        rb.buf[rb.head] = val;
-        rb.head = ring_wrap(rb.head + 1);
-    }
-
-    static int ring_dequeue(ring_t rb)
-    {
-        if (ring_empty(rb))
-        {
-            return -1;
-        }
-        // assert(!ring_empty(rb));
-        int slot_id = rb.buf[rb.tail];
-        rb.tail = ring_wrap(rb.tail + 1);
-        return slot_id;
-    }
-
-
     public static SApp.sapp_desc sokol_main()
     {
         return new SApp.sapp_desc()
@@ -445,6 +395,5 @@ public static unsafe partial class PlMpegApp
             icon = { sokol_default = true },
         };
     }
-
 
 }
